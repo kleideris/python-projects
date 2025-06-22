da9e060fc9db200e0507eb123a5b2b8dcf03b36b
Ch. Kleideris <kleiderisch@gmail.com>
2025-06-23 00:51:41 +0300

    feat: add student class with private fields and property decorators

---
+++ coding_factory/chapter2/student.py
@@ -0,0 +1,31 @@
+"""A simple Student class with private fields and property decorators"""
+
+class Student:
+    def __init__(self, id, firstname, lastname):
+        self.__id = id
+        self.__firstname = firstname
+        self.__lastname = lastname
+
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @property
+    def firstname(self):
+        return self.__firstname
+    
+    @firstname.setter
+    def firstname(self, firstname):
+        self.__firstname = firstname
+    
+    @property
+    def lastname(self):
+        return self.__lastname
+    
+    @lastname.setter
+    def lastname(self, lastname):
+        self.__lastname = lastname
